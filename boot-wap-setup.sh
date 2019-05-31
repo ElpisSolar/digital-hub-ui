a36f32276bd12d2576b89631392fed69d47581b9
samkellerhals <kellerhalssamuel@gmail.com>
2019-06-01 00:51:53 +0100

    add correct reboot script

--- boot-wap-setup.sh
+++ boot-wap-setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-mkdir ~/Desktop/test-directory
-mkdir ~/Desktop/test-directory2
+sleep 20s && /home/pi/digital-hub/kiwix-tools_linux-armhf-2019-05-31/kiwix-serve /media/pi/tresor-prt2/kiwix-content/*.zim --port=8078
+
+
