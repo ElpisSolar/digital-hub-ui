18842948dc5f894fe18a0b3f1c8ead1b211565cb
samkellerhals <kellerhalssamuel@gmail.com>
2019-06-01 00:55:59 +0100

    Revert "add correct reboot script"
    This reverts commit a36f32276bd12d2576b89631392fed69d47581b9.

--- boot-wap-setup.sh
+++ boot-wap-setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sleep 20s && /home/pi/digital-hub/kiwix-tools_linux-armhf-2019-05-31/kiwix-serve /media/pi/tresor-prt2/kiwix-content/*.zim --port=8078
-
-
+mkdir ~/Desktop/test-directory
+mkdir ~/Desktop/test-directory2
