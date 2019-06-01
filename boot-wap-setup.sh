e49da6d405ced94d8c450c3f18cebd2d3922b876
samkellerhals <kellerhalssamuel@gmail.com>
2019-06-01 01:07:54 +0100

    add final boot script

--- boot-wap-setup.sh
+++ boot-wap-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir ~/Desktop/test-directory
-mkdir ~/Desktop/test-directory2
+sleep 20s && sudo /home/pi/digital-hub/kiwix-tools_linux-armhf-2019-05-31/kiwix-serve /media/pi/tresor-prt2/kiwix-content/*.zim --port=8078
+
