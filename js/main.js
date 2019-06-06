f10e71e785e724f49cfddb8e7363f6c6b7fbc2bc
samkellerhals <kellerhalssamuel@gmail.com>
2019-06-06 08:37:14 +0100

    add language start-menu

--- js/main.js
+++ js/main.js
@@ -1,34 +1,22 @@
-/* Change language page content based on language settings */
-
+/* DOM elements */
 var frenchToggle = document.getElementById("french-toggle");
 var englishToggle = document.getElementById("english-toggle");
-var frenchActive = false;
-var englishActive = true;
+var startLink = document.getElementById("startLink");
 var langModal = document.getElementById("lang-modal");
-var conflict = false;
+var langNotice = document.getElementById("lang-notice");
 
-englishToggle.addEventListener("click", function(){
+/* state variables */
+var frenchActive = false;
+var englishActive = false;
 
-    var englishToggleBtn = englishToggle.childNodes[1];
+/* language notice html content*/
+var frenchNoticeContent = "<h6><b style='text-decoration: underline'>Remarque:</b> Le choix de la langue a une incidence sur le contenu qui vous est offert. Vous pouvez toujours changer la langue de la plate-forme dans Paramètres</em>.</h6>"
+var englishNoticeContent = "<h6><b style='text-decoration: underline'>Note:</b> Language choice impacts the content that is available to you. You can always change the platform language in <em>Settings</em>.</h6>"
+var englishSiteLink = "http://localhost/dev-digital-information-hub-rpi/start-en.html"
+var frenchSiteLink = "http://localhost/dev-digital-information-hub-rpi/start-fr.html"
 
-    if (englishToggleBtn.classList.contains("active")) {
-        console.log("english-active");
-        englishActive = true;
-    } else {
-        console.log("english-inactive");
-        englishActive = false;
-    }
-
-    if (englishActive == true && frenchActive == true) {
-        console.log("conflict");
-        conflict = true;
-        langModal.style.display = "block";
-    } else {
-        conflict = false;
-    }
-})
-
-frenchToggle.addEventListener("click", function(){
+/* language toggle event-listeners */
+frenchToggle.addEventListener("click", function () {
 
     var frenchToggleBtn = frenchToggle.childNodes[1];
 
@@ -41,17 +29,55 @@ frenchToggle.addEventListener("click", function(){
     }
 
     if (englishActive == true && frenchActive == true) {
-        console.log("conflict");
-        conflict = true;
         langModal.style.display = "block";
-    }  else {
-        conflict = false;
+        startLink.style.color = "gainsboro"
+        startLink.href = "";
+        langNotice.innerHTML = ""
+    } else if (englishActive == false && frenchActive == true) {
+        langNotice.innerHTML = frenchNoticeContent;
+        startLink.style.color = "#428bca"
+        startLink.href = frenchSiteLink;
+        langModal.style.display = "none";
+    } else if (englishActive == true && frenchActive == false) {
+        langNotice.innerHTML = englishNoticeContent;
+        startLink.style.color = "#428bca"
+        startLink.href = englishSiteLink;
+        langModal.style.display = "none"
+    } else if (englishActive == false && frenchActive == false) {
+        langNotice.innerHTML = ""
+        startLink.href = "";
+        startLink.style.color = "gainsboro"
     }
-
 })
 
+englishToggle.addEventListener("click", function () {
 
+    var englishToggleBtn = englishToggle.childNodes[1];
 
+    if (englishToggleBtn.classList.contains("active")) {
+        englishActive = true;
+    } else {
+        englishActive = false;
+    }
 
-
-/* add conditional statement where if both toggles are active modal appears. */
+    if (englishActive == true && frenchActive == true) {
+        langModal.style.display = "block";
+        langNotice.innerHTML = ""
+        startLink.href = "";
+        startLink.style.color = "gainsboro"
+    } else if (englishActive == false && frenchActive == true) {
+        langNotice.innerHTML = frenchNoticeContent;
+        startLink.style.color = "#428bca"
+        startLink.href = frenchSiteLink;
+        langModal.style.display = "none";
+    } else if (englishActive == true && frenchActive == false) {
+        langNotice.innerHTML = englishNoticeContent;
+        startLink.style.color = "#428bca"
+        startLink.href = englishSiteLink;
+        langModal.style.display = "none";
+    } else if (englishActive == false && frenchActive == false) {
+        langNotice.innerHTML = ""
+        startLink.href = "";
+        startLink.style.color = "gainsboro"
+    }
+})
