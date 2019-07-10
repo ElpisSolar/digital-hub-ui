2e85d9e8c62e68c8fb25c524391c6fc34f58a953
samkellerhals <kellerhalssamuel@gmail.com>
2019-07-10 20:23:43 +0200

    production v1

--- boot-wap-setup.sh
+++ boot-wap-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sleep 20s && sudo /home/pi/digital-hub/kiwix-tools_linux-armhf-2019-05-31/kiwix-serve /media/pi/tresor-prt2/kiwix-content/*.zim --port=8078
+sleep 20s && sudo /home/pi/digital-hub/kiwix-tools_linux-armhf-2019-05-31/kiwix-serve /home/pi/digital-hub/kiwix-content/*.zim --port=8078
 
