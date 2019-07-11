00ed00037e04c949bd8ea771f3ead13f5171b5d3
samkellerhals <kellerhalssamuel@gmail.com>
2019-07-11 20:47:58 +0200

    add up to date kiwix-tools

--- boot-wap-setup.sh
+++ boot-wap-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sleep 20s && sudo /home/pi/digital-hub/kiwix-tools_linux-armhf-2019-05-31/kiwix-serve /home/pi/digital-hub/kiwix-content/*.zim --port=8078
+sleep 5s && sudo /home/pi/digital-hub/kiwix-tools_linux-armhf-2019-07-10/kiwix-serve /home/pi/digital-hub/kiwix-content/*.zim --port=8078
 
